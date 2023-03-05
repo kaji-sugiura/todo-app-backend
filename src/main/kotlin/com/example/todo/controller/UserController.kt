859fb57c134db2048af8fb7df98b3714d8f92fe1
kaji-sugiura <kaji.sugiura@gmail.com>
2023-03-05 20:24:38 +0900

    exception handler実装

--- src/main/kotlin/com/example/todo/controller/UserController.kt
+++ src/main/kotlin/com/example/todo/controller/UserController.kt
@@ -5,6 +5,7 @@ import com.example.todo.entity.User
 import com.example.todo.service.UserService
 import jakarta.validation.Valid
 import org.springframework.http.HttpStatus
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController
  */
 @RestController
 @RequestMapping("/api/v1/user")
+@Validated
 class UserController(val userService: UserService) {
 
     /**
